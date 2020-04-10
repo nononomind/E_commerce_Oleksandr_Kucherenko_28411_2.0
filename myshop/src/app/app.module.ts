ff0112acb577fb593d90341a8c7c76facc59c22a
Olexandr <SashaKucherenkoGG@gmail.com>
2020-04-11 00:16:57 +0200

    Laboratorium 4

---
+++ myshop/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PanelTopComponent } from './components/panel-top/panel-top.component';
+import { CartComponent } from './components/cart/cart.component';
+
+const appRoutes: Routes = [
+  { path: 'cart', component: CartComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    ProductsComponent,
+    PanelTopComponent,
+    CartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
