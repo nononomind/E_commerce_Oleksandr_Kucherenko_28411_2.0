d21fdaf87e01affda7fbbf05ed5c339659dc9947
Olexandr <SashaKucherenkoGG@gmail.com>
2020-06-02 10:04:45 +0200

    Laboratorium 7

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -12,6 +12,10 @@ import { CartComponent } from './components/cart/cart.component';
 import { DashboardComponent } from './components/admin/dashboard/dashboard.component';
 import { MenuComponent } from './components/admin/menu/menu.component';
 import { AdmProductsComponent } from './components/admin/adm-products/adm-products.component';
+import { OrdersComponent } from './components/admin/orders/orders.component';
+
+import { ChartsModule } from 'ng2-charts';
+
 
 const appRoutes: Routes = [
   { path: 'cart', component: CartComponent },
@@ -19,6 +23,7 @@ const appRoutes: Routes = [
   { path: 'landing', component: LandingComponent },
   { path: 'adm_dashboard', component: DashboardComponent},
   { path: 'adm_products', component: AdmProductsComponent},
+  { path: 'adm_orders', component: OrdersComponent},
   { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
 
@@ -31,13 +36,15 @@ const appRoutes: Routes = [
     CartComponent,
     DashboardComponent,
     MenuComponent,
-    AdmProductsComponent
+    AdmProductsComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
